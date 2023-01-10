6a317aec0a4e0561717a436148944182e3da41ac
Amili-usain <amilihussein073@gmail.com>
2023-01-10 15:17:58 -0800

    tasks 0-2

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file using Puppet
+
+file { '/tmp/holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+}
