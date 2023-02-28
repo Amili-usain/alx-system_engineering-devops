b8c613d2d3bac53e8149952604a3e176e81b3fa6
Amili-usain <amilihussein073@gmail.com>
2023-02-28 01:57:13 -0800

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Prints the titles of the first 10 hot posts listed for a given
+    subreddit.
+    """
+    # Set the Default URL strings
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/hot.json'.format(base=base_url,
+                                                     subreddit=subreddit)
+
+    # Set an User-Agent
+    user_agent = {'User-Agent': 'Python/requests'}
+
+    # Set the Query Strings to Request
+    payload = {'limit': '10'}
+
+    # Get the Response of the Reddit API
+    res = requests.get(api_uri, headers=user_agent,
+                       params=payload, allow_redirects=False)
+
+    # Checks if the subreddit is invalid
+    if res.status_code in [302, 404]:
+        print('None')
+    else:
+        res_json = res.json()
+
+        if res_json.get('data') and res_json.get('data').get('children'):
+            # Get the 10 hot posts of the subreddit
+            hot_posts = res_json.get('data').get('children')
+
+            # Print each hot post title
+            for post in hot_posts:
+                if post.get('data') and post.get('data').get('title'):
+                    print(post.get('data').get('title'))
